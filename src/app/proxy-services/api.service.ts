531d2debf08da459797004958ba7f60775d7ea19
dev-ce01 <127472367+dev-ce01@users.noreply.github.com>
2023-11-01 06:08:33 -0700

    Update api.service.ts

--- src/app/proxy-services/api.service.ts
+++ src/app/proxy-services/api.service.ts
@@ -7,7 +7,7 @@ import { Keys } from './Keys';
   providedIn: 'root',
 })
 export class ApiService {
-  private baseUrl = 'https://8a75-223-123-4-103.ngrok-free.app'; 
+  private baseUrl = 'http://85.31.232.34:500'; 
 
   username: string ="";
   password: string="";
